e27c298d4bb05db91d5b74634f4aac3f7cbc05eb
LYH666\26487 <2648782102@qq.com>
2025-12-05 00:48:26 +0800

    修复图标库报错

--- app/plugins/fontawesome.ts
+++ app/plugins/fontawesome.ts
@@ -1,11 +1,79 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import {
+  faFileText,
+  faSun,
+  faMoon,
+  faRightFromBracket,
+  faRightToBracket,
+  faHome,
+  faPlus,
+  faUser,
+  faSave,
+  faTrash,
+  faHeading,
+  faBold,
+  faItalic,
+  faCode,
+  faStrikethrough,
+  faList,
+  faListOl,
+  faQuoteRight,
+  faTable,
+  faMinus,
+  faAsterisk,
+  faHighlighter,
+  faSuperscript,
+  faSubscript,
+  faLink,
+  faImage,
+  faEdit,
+  faXmark,
+  faArrowLeft,
+  faFolder,
+  faSearch,
+  faSpinner
+} from '@fortawesome/free-solid-svg-icons'
 
 // 配置 Font Awesome
-import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
-// 只注册组件，不添加任何图标
-// 图标将在使用时直接导入
+// 添加常用图标到库
+library.add(
+  faFileText,
+  faSun,
+  faMoon,
+  faRightFromBracket,
+  faRightToBracket,
+  faHome,
+  faPlus,
+  faUser,
+  faSave,
+  faTrash,
+  faHeading,
+  faBold,
+  faItalic,
+  faCode,
+  faStrikethrough,
+  faList,
+  faListOl,
+  faQuoteRight,
+  faTable,
+  faMinus,
+  faAsterisk,
+  faHighlighter,
+  faSuperscript,
+  faSubscript,
+  faLink,
+  faImage,
+  faEdit,
+  faXmark,
+  faArrowLeft,
+  faFolder,
+  faSearch,
+  faSpinner
+)
+
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)
 })
